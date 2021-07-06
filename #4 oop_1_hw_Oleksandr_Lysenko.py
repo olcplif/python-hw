7a9513f278b2edd82cbb757dfcacb9cb69042eca
Oleksandr Lysenko <oleksandr.lysenko.jr@gmail.com>
2021-07-06 23:36:41 +0300

    OOP-1 from home_2

--- #4 oop_1_hw_Oleksandr_Lysenko.py
+++ #4 oop_1_hw_Oleksandr_Lysenko.py
@@ -17,8 +17,8 @@ class Bus(Vehicle):
 
 
 # 3. Determine which class a given Bus object belongs to (Check type of an object)
-mersedes = Bus(50, 140, 500000)
-print("Type of an object -", type(mersedes))
+mercedes = Bus(50, 140, 500000)
+print("Type of an object -", type(mercedes))
 
 # 4. Determine if School_bus is also an instance of the Vehicle class
 School_bus = Bus(55, 70, 355000)
@@ -31,6 +31,7 @@ class School:
         self.get_school_id = get_school_id
         self.number_of_students = number_of_students
 
+
 # 6*. Create a new class SchoolBus that will inherit all of the methods from School and Bus and will have its own -
 # bus_school_color
 class SchoolBus(School, Bus):
@@ -39,7 +40,69 @@ class SchoolBus(School, Bus):
         self.color = color
 
     def bus_school_color(self):
-            pass
+        pass
+
 
 # 7. Polymorphism: Create two classes: Bear, Wolf. Both of them should have make_sound method. Create two instances,
 # one of Bear and one of Wolf, make a tuple of it and by using for call their action using the same method.
+
+
+class Bear:
+    def make_sound(self):
+       print('Grrrr')
+
+
+class Wolf:
+    def make_sound(self):
+        print('Owooo')
+
+
+bear = Bear()
+wolf = Wolf()
+animals = (bear, wolf)
+
+for animal in animals:
+    animal.make_sound()
+
+# 8. Create class City with name, population instance attributes, return a new instance only when population > 1500,
+# otherwise return message: "Your city is too small".
+# 9. Override a printable string representation of the City class and return:
+# The population of the city {name} is {population}
+
+class City:
+    def __init__(self, name, population):
+        self.name = name
+        self.population = population
+
+    def check_population(self):
+        if self.population > 1500:
+            return f'The population of the city {self.name} is {self.population}'
+        else:
+            return 'Your city is too small'
+
+
+ivano_frankivsk = City("Ivano-Frankivsk", 25000)
+mykytynci = City("Mykytynci", 1000)
+print(ivano_frankivsk.check_population())
+print(mykytynci.check_population())
+
+# 10*. Override magic method __add__() to perform the additional action as 'multiply' (*)
+# the value which is greater than 10. And perform this add (+) of two instances.
+
+
+class Addition:
+    def __init__(self, addition):
+        self.addition = addition
+
+    def __add__(self, other):
+        if self.addition > 10 or other.addition > 10:
+            return self.addition * other.addition
+        else:
+            return self.addition + other.addition
+
+
+x = Addition(11)
+y = Addition(10)
+add = x + y
+print(add)
+
