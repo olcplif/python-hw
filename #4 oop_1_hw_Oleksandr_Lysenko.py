75ed608fd8c16931379768759606a1c574c276c9
Oleksandr Lysenko <oleksandr.lysenko.jr@gmail.com>
2021-07-07 00:31:30 +0300

    hw oop-1_done!

--- #4 oop_1_hw_Oleksandr_Lysenko.py
+++ #4 oop_1_hw_Oleksandr_Lysenko.py
@@ -19,10 +19,14 @@ class Bus(Vehicle):
 # 3. Determine which class a given Bus object belongs to (Check type of an object)
 mercedes = Bus(50, 140, 500000)
 print("Type of an object -", type(mercedes))
+# OUTPUT: Type of an object - <class '__main__.Bus'>
 
 # 4. Determine if School_bus is also an instance of the Vehicle class
 School_bus = Bus(55, 70, 355000)
 print("School_bus is instance of Vehicle class -", isinstance(School_bus, Vehicle))
+
+
+# OUTPUT: School_bus is instance of Vehicle class - True
 
 
 # 5. Create a new class School with get_school_id and number_of_students instance attributes
@@ -49,7 +53,7 @@ class SchoolBus(School, Bus):
 
 class Bear:
     def make_sound(self):
-       print('Grrrr')
+        print('Grrrr')
 
 
 class Wolf:
@@ -63,6 +67,11 @@ animals = (bear, wolf)
 
 for animal in animals:
     animal.make_sound()
+
+
+# OUTPUT: Grrrr
+# Owooo
+
 
 # 8. Create class City with name, population instance attributes, return a new instance only when population > 1500,
 # otherwise return message: "Your city is too small".
@@ -84,7 +93,11 @@ class City:
 ivano_frankivsk = City("Ivano-Frankivsk", 25000)
 mykytynci = City("Mykytynci", 1000)
 print(ivano_frankivsk.check_population())
+# OUTPUT: The population of the city Ivano-Frankivsk is 25000
 print(mykytynci.check_population())
+
+
+# OUTPUT: Your city is too small
 
 # 10*. Override magic method __add__() to perform the additional action as 'multiply' (*)
 # the value which is greater than 10. And perform this add (+) of two instances.
@@ -106,6 +119,9 @@ y = Addition(10)
 add = x + y
 print(add)
 
+
+# OUTPUT: 110
+
 # 11. The __call__ method enables Python programmers to write classes where the instances behave like functions
 # and can be called like a function.
 # Create a new class with __call__ method and define this call to return sum.
@@ -116,16 +132,37 @@ class Sum:
         return a + b
 
 
-sum = Sum()
-print(sum(5, 4))
+summ = Sum()
+print(summ(5, 4))
 
-#12*. Making Your Objects Truthy or Falsey Using __bool__().
-#Create class MyOrder with cart and customer instance attributes.
-#Override the __bool__magic method considered to be truthy if the length of the cart list is non-zero.
-#e.g.:
-#order_1 = MyOrder(['a', 'b', 'c'], 'd')
-#order_2 = MyOrder([], 'a')
-#bool(order_1)
-#True
-#bool(order_2)
-#False
+
+# OUTPUT: 9
+
+# 12*. Making Your Objects Truthy or Falsey Using __bool__().
+# Create class MyOrder with cart and customer instance attributes.
+# Override the __bool__magic method considered to be truthy if the length of the cart list is non-zero.
+# e.g.:
+# order_1 = MyOrder(['a', 'b', 'c'], 'd')
+# order_2 = MyOrder([], 'a')
+# bool(order_1)
+# True
+# bool(order_2)
+# False
+
+
+class MyOrder:
+    def __init__(self, cart, customer):
+        self.cart = cart
+        self.customer = customer
+
+    def __bool__(self):
+        if len(self.cart) != 0:
+            return True
+        else:
+            return False
+
+
+order_1 = MyOrder(['a', 'b', 'c'], 'd')
+order_2 = MyOrder([], 'a')
+print(bool(order_1))  # OUTPUT: True
+print(bool(order_2))  # False
