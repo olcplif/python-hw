234fa6c720fbb30cb8060c024f1b352b512d71f3
Oleksandr Lysenko <oleksandr.lysenko.jr@gmail.com>
2021-08-05 22:42:35 +0300

    Task#2 Ready

--- #13_Decorators/Task_2.py
+++ #13_Decorators/Task_2.py
@@ -3,5 +3,20 @@
 # Важливо! Функція повинно повернути саме значення гіпотенузи як число, а декоратор повинен зробити вивід
 # на екран наприклад такого тексту “При катетах 3, 4 – гіпотенуза дорівнює 5”.
 
-def func():
-    pass
+
+def print_hypotenuse(func):
+    def wrap(leg_1, leg_2):
+        result = func(leg_1, leg_2)
+        print(f"At legs {leg_1} and {leg_2} the hypotenuse is equal to {result}")
+        return result
+
+    return wrap
+
+
+@print_hypotenuse
+def get_hypotenuse(leg_1, leg_2):
+    hypotenuse = (((leg_1 ** 2) + (leg_2 ** 2)) ** 0.5)
+    return hypotenuse
+
+
+get_hypotenuse(3, 4)
