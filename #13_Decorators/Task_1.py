7c664b182f5c942f05beb19460a4d684ef9075a2
Oleksandr Lysenko <oleksandr.lysenko.jr@gmail.com>
2021-08-05 22:25:26 +0300

    Task#1 Ready

--- #13_Decorators/Task_1.py
+++ #13_Decorators/Task_1.py
@@ -1,6 +1,28 @@
 # 1. Написати функцію яка в циклі зчитує з консолі введені користувачем дані і записує їх в список.
 # Написати декоратор який видасть час виконання роботи функції
+import time
 
-def func():
-    pass
 
+def check_time(func):
+    def wrap(*args, **kwargs):
+        time_start = time.time()
+        result = func(*args, **kwargs)
+        print(f">> This function was performed for {time.time() - time_start} seconds. <<")
+        return result
+
+    return wrap
+
+
+@check_time
+def create_list():
+    print('Enter "stop" for exit.')
+    list_var = []
+    while True:
+        var = input('Enter data: ')
+        if var == 'stop':
+            break
+        list_var.append(var)
+    return list_var
+
+
+print(create_list())
