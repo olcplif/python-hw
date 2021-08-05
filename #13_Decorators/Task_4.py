4ebe20152cef10d701bb7f1c9d32dfa010eb6661
Oleksandr Lysenko <oleksandr.lysenko.jr@gmail.com>
2021-08-06 00:21:49 +0300

    Task#4 Ready

--- #13_Decorators/Task_4.py
+++ #13_Decorators/Task_4.py
@@ -2,5 +2,23 @@
 # Написати до цієї функції декоратор який всі елементи отриманого списку переведе в строковий тип даних
 
 
-def func():
-    pass
+def convert_to_str(func):
+    def wrap(n):
+        str_list = []
+        for el in func(n):
+            el = str(el)
+            str_list.append(el)
+        return str_list
+
+    return wrap
+
+
+@convert_to_str
+def create_list(n):
+    list_ = []
+    for el in range(n + 1):
+        list_.append(el)
+    return list_
+
+
+print(create_list(7))
