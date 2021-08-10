3d688a995dd1b6878001efb77db2f1daaa5ce4a4
Oleksandr Lysenko <oleksandr.lysenko.jr@gmail.com>
2021-08-10 23:56:10 +0300

    Task#4_Ready_1

--- #14_Decorators/#14_Task_3.py
+++ #14_Decorators/#14_Task_3.py
@@ -11,7 +11,8 @@
 # <p> Vitaliy </p>
 # <p> Vita </p>
 #
-# До даної функції напишіть декоратор який загорне отриману строку в div блок. style_class - це аргумент який приймається
+# До даної функції напишіть декоратор який загорне отриману строку в div блок.
+# style_class - це аргумент який приймається
 # декоратором, тобто декоратор або в 3 рівні, або функтор (краще зробіть через функтор)
 #
 # <div class=*style_class*>
@@ -22,18 +23,25 @@
 # <p> Vita </p>
 # </div>
 
+class MakeDiv:
+    def __init__(self, style_class):
+        self.style_class = style_class
 
-from functools import wraps
+    def __call__(self, func):
+        def wrap(*args):
+            result = f"<div class = {self.style_class}>" + "\n" + func(*args) + "<div>"
+            return result
+
+        return wrap
 
 
-def decor(func):
-    @wraps(func)
-    def wrap(*args, **kwargs):
-        pass
-    return wrap
-
-
-@decor
+@MakeDiv('*style_class*')
 def get_names_page(names_list):
     template_head = "<h3> User names: </h3>"
-    template = "<p> {} </p>"
+    template = ""
+    for name in names_list:
+        template = template + f"<p> {name} </p>" + "\n"
+    return template_head + "\n" + template
+
+
+print(get_names_page(["Misha", "Olya", "Vitaliy", "Vita"]))
