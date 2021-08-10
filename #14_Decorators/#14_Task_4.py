fcafd302953cfe3ae0d958b92d04c0cb0995d8d6
Oleksandr Lysenko <oleksandr.lysenko.jr@gmail.com>
2021-08-11 00:32:29 +0300

    Task#4_Ready_1

---
+++ #14_Decorators/#14_Task_4.py
@@ -0,0 +1,126 @@
+# 4. Допишіть ще один декоратор, який загорне результат роботи з попереднього завдання в теги <body> </body>.
+# Щоб отримати ось такий код
+#
+# <body>
+# <div class=*style_class*>
+# <h3> User names: </h3>
+# <p> Misha </p>
+# <p> Olya </p>
+# <p> Vitaliy </p>
+# <p> Vita </p>
+# </div>
+# </body>
+#
+# ще один декоратор який приклеїть до існуючого куска html - head блок,
+# де *title* це аргумент що отримується декоратором
+#
+# <head>
+# <title>*title*</title>
+# </head>
+# <body>
+# <div class=*style_class*>
+# <h3> User names: </h3>
+# <p> Misha </p>
+# <p> Olya </p>
+# <p> Vitaliy </p>
+# <p> Vita </p>
+# </div>
+# </body>
+#
+# Ще один декоратор який загорне все що є в <html> </html> теги
+#
+# <html>
+# <head>
+# <title>*title*</title>
+# </head>
+# <body>
+# <div class=*style_class*>
+# <h3> User names: </h3>
+# <p> Misha </p>
+# <p> Olya </p>
+# <p> Vitaliy </p>
+# <p> Vita </p>
+# </div>
+# </body>
+# </html>
+#
+# Зверніть увагу що у вас на одну функцію повинно назначатись 4 декоратори, тому потрібно не тільки їх написати, а й
+# вибрати правильний порядок декорування щоб після всіх декорацій отримати наприклад ось такий html
+# Також деякі декоратори доцільно буде робити через функції в "2 рівні", деякі в "3 рівні", деякі через функтори
+#
+# <html>
+# <head>
+# <title>Users</title>
+# </head>
+# <body>
+# <div class=users_block>
+# <h3> User names: </h3>
+# <p> Misha </p>
+# <p> Olya </p>
+# <p> Vitaliy </p>
+# <p> Vita </p>
+# </div>
+# </body>
+# </html>
+
+class MakeDivTag:
+    def __init__(self, style_class):
+        self.style_class = style_class
+
+    def __call__(self, func):
+        def wrap(*args):
+            result = f"<div class={self.style_class}>" + "\n" + func(*args) + "</div>"
+            return result
+
+        return wrap
+
+
+class MakeBodyTag:
+
+    def __call__(self, func):
+        def wrap(*args):
+            result = f"<body>" + "\n" + func(*args) + "\n" + "</body>"
+            return result
+
+        return wrap
+
+
+class MakeHeadTag:
+    def __init__(self, title):
+        self.title = title
+
+    def __call__(self, func):
+        def wrap(*args):
+            result = f"<head>\n<title>{self.title}</title>\n</head>" + "\n" + func(*args)
+            return result
+
+        return wrap
+
+
+class MakeHtmlTag:
+
+    def __call__(self, func):
+        def wrap(*args):
+            result = f"<html>" + "\n" + func(*args) + "\n" + "</html>"
+            return result
+
+        return wrap
+
+
+@MakeHtmlTag()
+@MakeHeadTag('Users')
+@MakeBodyTag()
+@MakeDivTag('users_block')
+def get_names_page(names_list):
+    template_head = "<h3> User names: </h3>"
+    template = ""
+    for name in names_list:
+        template = template + f"<p> {name} </p>" + "\n"
+    return template_head + "\n" + template
+
+
+page_user_name = get_names_page(["Misha", "Olya", "Vitaliy", "Vita"])
+print(page_user_name)
+
+with open("user_name.html", "w") as file:
+    print(file.write(page_user_name))
